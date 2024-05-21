264eccc9808f8103805368e8b16fd2cb9d76252f
sgirishkumar04 <159610805+sgirishkumar04@users.noreply.github.com>
2024-05-21 06:53:29 +0530

    Add files via upload

---
+++ NFA_to_DFA_main.py
@@ -0,0 +1,374 @@
+import graphviz
+import collections
+import json
+
+def prepareForDrawing(states, end_state, prev_start):
+    # make the last state as out state
+    states["S" + str(end_state)]["terminalState"] = True
+    # sort the state ascending
+    states = collections.OrderedDict(sorted(states.items()))
+    # loop over sorted states and save them as the given example to json file
+    # return the json file content to be displayed in graph format
+    states.update({"startingState": ("S" + str(prev_start))})
+    with open('out/nfa.json', 'w') as fp:
+        json.dump(states, fp, ensure_ascii=True)
+    print(states)
+    return states
+
+def construct_node(state, nfa, starting_state, graph):
+    # construct a graph node using given state
+    # check whether state is terminal to draw double circle
+    if nfa[state]['terminalState']:
+        graph.node(state, label=state, shape='doublecircle')
+    else:
+        graph.node(state, label=state, shape='circle')
+    # check whether state is starting to draw input arrow
+    if state == starting_state:
+        graph.edge('start', state)
+
+def visualize(nfa):
+    # visualize output NFA into a directed graph
+    # initialize directed graph
+    graph = graphviz.Digraph(comment='NFA Visualization')
+    # set graph flow from left to right
+    graph.graph_attr['rankdir'] = 'LR'
+    # set entry point
+    graph.node('start', label='start', shape='plaintext')
+    # get the starting state of NFA
+    starting_state = nfa['startingState']
+    del nfa['startingState']
+    # initialize nodes list
+    nodes = list()
+    # loop over each NFA state
+    for state in nfa:
+        # check whether state is created or not
+        if not state in nodes:
+            # add state to created nodes
+            nodes.append(state)
+            # construct a graph node for the state
+            construct_node(state, nfa, starting_state, graph)
+        # loop over each successor state
+        for successor in nfa[state]:
+            # skip 'terminalstate' key
+            if successor != 'terminalState':
+                # check whether successor state is created or not
+                if nfa[state][successor] in nodes:
+                    # create an edge between two states
+                    graph.edge(state, nfa[state][successor], label=successor)
+                else:
+                    # add state to created nodes
+                    nodes.append(nfa[state][successor])
+                    # construct a graph node for the state
+                    construct_node(nfa[state][successor], nfa, starting_state, graph)
+                    # create an edge between two states
+                    graph.edge(state, nfa[state][successor], label=successor)
+    # set output format to SVG
+    graph.format = 'svg'
+    # render final graph
+    graph.render('out/nfa-graph', view=False)
+
+class NFA:
+    def __init__(self, no_state, states, no_alphabet, alphabets, start,
+                 no_final, finals, no_transition, transitions):
+        self.no_state = no_state
+        self.states = states
+        self.no_alphabet = no_alphabet
+        self.alphabets = alphabets
+
+        # Adding epsilon alphabet to the list
+        # and incrementing the alphabet count
+        self.alphabets.append('e')
+        self.no_alphabet += 1
+        self.start = start
+        self.no_final = no_final
+        self.finals = finals
+        self.no_transition = no_transition
+        self.transitions = transitions
+        self.graph = graphviz.Digraph()
+
+        # Dictionaries to get index of states or alphabets
+        self.states_dict = dict()
+        for i in range(self.no_state):
+            self.states_dict[self.states[i]] = i
+        self.alphabets_dict = dict()
+        for i in range(self.no_alphabet):
+            self.alphabets_dict[self.alphabets[i]] = i
+
+        # transition table is of the form
+        # [From State + Alphabet pair] -> [Set of To States]
+        self.transition_table = dict()
+        for i in range(self.no_state):
+            for j in range(self.no_alphabet):
+                self.transition_table[str(i) + str(j)] = []
+        for i in range(self.no_transition):
+            self.transition_table[str(self.states_dict[self.transitions[i][0]])
+                                  + str(self.alphabets_dict[
+                                      self.transitions[i][1]])].append(
+                self.states_dict[self.transitions[i][2]])
+
+    # Method to get input from User
+    @classmethod
+    def fromUser(cls):
+        with open("output.json", 'r') as file:
+            nfa_json = json.load(file)
+            
+        no_state = nfa_json["no_state"]
+        states = nfa_json["states"]
+        no_alphabet = nfa_json["no_alphabet"]
+        alphabets = nfa_json["alphabets"]
+        start = nfa_json["start"]
+        no_final = nfa_json["no_final"]
+        finals = nfa_json["finals"]
+        no_transition = nfa_json["no_transition"]
+        transitions = nfa_json["transitions"]
+        return cls(no_state, states, no_alphabet, alphabets, start,
+                   no_final, finals, no_transition, transitions)
+
+    # Method to represent quintuple
+    def __repr__(self):
+        return "Q : " + str(self.states) + "\nΣ : " + str(self.alphabets) + "\nq0 : " + str(self.start) + "\nF : " + str(
+            self.finals) + "\nδ : \n" + str(self.transition_table)
+
+    def getEpsilonClosure(self, state):
+
+        # Method to get Epsilon Closure of a state of NFA
+        # Make a dictionary to track if the state has been visited before
+        # And a array that will act as a stack to get the state to visit next
+        closure = dict()
+        closure[self.states_dict[state]] = 0
+        closure_stack = [self.states_dict[state]]
+
+        # While stack is not empty the loop will run
+        while (len(closure_stack) > 0):
+
+            # Get the top of stack that will be evaluated now
+            cur = closure_stack.pop(0)
+
+            # For the epsilon transition of that state,
+            # if not present in closure array then add to dict and push to stack
+            for x in self.transition_table[
+                str(cur) + str(self.alphabets_dict['e'])]:
+                if x not in closure.keys():
+                    closure[x] = 0
+                    closure_stack.append(x)
+            closure[cur] = 1
+        return closure.keys()
+
+    def getStateName(self, state_list):
+        if -1 in state_list:
+            return 'ϕ'
+        else:
+            # Get name from set of states to display in the final DFA diagram
+            name = ''
+            for x in state_list:
+                name += self.states[x]
+            return name
+
+    def isFinalDFA(self, state_list):
+
+        # Method to check if the set of state is final state in DFA
+        # by checking if any of the set is a final state in NFA
+        for x in state_list:
+            for y in self.finals:
+                if (x == self.states_dict[y]):
+                    return True
+        return False
+
+
+print("E-NFA to DFA")
+
+# Uncomment the following two lines to get input from the user
+# nfa = NFA.fromUser() # To get input from user
+# print(repr(nfa)) # To print the quintuple in console
+
+# Uncomment the following two lines to get input from the user
+nfa = NFA.fromUser()  # To get input from user
+print(repr(nfa))  # To print the quintuple in console
+
+# Making an object of Digraph to visualize NFA diagram
+nfa.graph = graphviz.Digraph()
+
+# Adding states/nodes in NFA diagram
+for x in nfa.states:
+    # If state is not a final state, then border shape is single circle
+    # Else it is double circle
+    if (x not in nfa.finals):
+        nfa.graph.attr('node', shape='circle')
+        nfa.graph.node(x)
+    else:
+        nfa.graph.attr('node', shape='doublecircle')
+        nfa.graph.node(x)
+
+# Adding start state arrow in NFA diagram
+nfa.graph.attr('node', shape='none')
+nfa.graph.node('')
+nfa.graph.edge('', nfa.start)
+
+# Adding edge between states in NFA from the transitions array
+for x in nfa.transitions:
+    nfa.graph.edge(x[0], x[2], label=('ε', x[1])[x[1] != 'e'])
+
+# Makes a pdf with name nfa.graph.pdf and views the pdf
+nfa.graph.render('nfa', view=True)
+
+# Making an object of Digraph to visualize DFA diagram
+dfa = graphviz.Digraph()
+
+# Finding epsilon closure beforehand so to not recalculate each time
+epsilon_closure = dict()
+for x in nfa.states:
+    epsilon_closure[x] = list(nfa.getEpsilonClosure(x))
+
+# First state of DFA will be epsilon closure of start state of NFA
+# This list will act as stack to maintain till when to evaluate the states
+dfa_stack = list()
+dfa_stack.append(epsilon_closure[nfa.start])
+
+# Check if start state is the final state in DFA
+if (nfa.isFinalDFA(dfa_stack[0])):
+    dfa.attr('node', shape='doublecircle')
+else:
+    dfa.attr('node', shape='circle')
+dfa.node(nfa.getStateName(dfa_stack[0]))
+
+# Adding start state arrow to start state in DFA
+dfa.attr('node', shape='none')
+dfa.node('')
+dfa.edge('', nfa.getStateName(dfa_stack[0]))
+
+# List to store the states of DFA
+dfa_states = list()
+dfa_states.append(epsilon_closure[nfa.start])
+
+# Loop will run till this stack is not empty
+while (len(dfa_stack) > 0):
+    # Getting top of the stack for current evaluation
+    cur_state = dfa_stack.pop(0)
+
+    # Traversing through all the alphabets for evaluating transitions in DFA
+    for al in range((nfa.no_alphabet) - 1):
+        # Set to see if the epsilon closure of the set is empty or not
+        from_closure = set()
+        for x in cur_state:
+            # Performing Union update and adding all the new states in set
+            from_closure.update(
+                set(nfa.transition_table[str(x) + str(al)]))
+
+        # Check if epsilon closure of the new set is not empty
+        if (len(from_closure) > 0):
+            # Set for the To state set in DFA
+            to_state = set()
+            for x in list(from_closure):
+                to_state.update(set(epsilon_closure[nfa.states[x]]))
+
+            # Check if the to state already exists in DFA and if not then add it
+            if list(to_state) not in dfa_states:
+                dfa_stack.append(list(to_state))
+                dfa_states.append(list(to_state))
+
+                # Check if this set contains final state of NFA
+                # to get if this set will be final state in DFA
+                if (nfa.isFinalDFA(list(to_state))):
+                    dfa.attr('node', shape='doublecircle')
+                else:
+                    dfa.attr('node', shape='circle')
+                dfa.node(nfa.getStateName(list(to_state)))
+
+            # Adding edge between from state and to state
+            dfa.edge(nfa.getStateName(cur_state),
+                     nfa.getStateName(list(to_state)),
+                     label=nfa.alphabets[al])
+
+        # Else case for empty epsilon closure
+        # This is a dead state(ϕ) in DFA
+        else:
+
+            # Check if any dead state was present before this
+            # if not then make a new dead state ϕ
+            if (-1) not in dfa_states:
+                dfa.attr('node', shape='circle')
+                dfa.node('ϕ')
+
+                # For new dead state, add all transitions to itself,
+                # so that machine cannot leave the dead state
+                for alpha in range(nfa.no_alphabet - 1):
+                    dfa.edge('ϕ', 'ϕ', nfa.alphabets[alpha])
+
+                # Adding -1 to list to mark that dead state is present
+                dfa_states.append(-1)
+
+            # Adding transition to dead state
+            dfa.edge(nfa.getStateName(cur_state, ),
+                     'ϕ', label=nfa.alphabets[al])
+
+# Makes a pdf with name dfa.pdf and views
+dfa.render('dfa', view=True)
+
+# Display transition table for DFA
+print("\nTransition Table for DFA:")
+print("{:<10} |".format(""), end="")
+for alphabet in nfa.alphabets[:-1]:
+    print("{:<10} |".format(alphabet), end="")
+print("\n" + "-" * (12 * len(nfa.alphabets)))
+
+# Print each row of the transition table
+for state_index, state in enumerate(dfa_states):
+    if state == -1:
+        # Skip printing transitions for the dead state
+        continue
+
+    print("{:<10} |".format(nfa.getStateName(state)), end="")
+    for alphabet_index in range(nfa.no_alphabet - 1):
+        alphabet = nfa.alphabets[alphabet_index]
+        from_closure = set()
+        for x in state:
+            from_closure.update(
+                set(nfa.transition_table[str(x) + str(alphabet_index)]))
+        to_state = set()
+        for x in list(from_closure):
+            to_state.update(set(epsilon_closure[nfa.states[x]]))
+        
+        # Check if the to_state is the dead state 'ϕ'
+        if len(to_state) == 0:
+            print("{:<10} |".format('ϕ'), end="")
+        else:
+            print("{:<10} |".format(nfa.getStateName(to_state)), end="")
+    print()
+
+# Display transition functions for each state in the DFA
+for state_index, state in enumerate(dfa_states):
+    # Skip printing transitions for the dead state
+    if state == -1:
+        continue
+
+    # Print transition functions for each alphabet
+    for alphabet_index in range(nfa.no_alphabet - 1):
+        alphabet = nfa.alphabets[alphabet_index]
+        from_closure = set()
+        for x in state:
+            from_closure.update(
+                set(nfa.transition_table[str(x) + str(alphabet_index)]))
+        to_state = set()
+        for x in list(from_closure):
+            to_state.update(set(epsilon_closure[nfa.states[x]]))
+        
+        # Format the state names for printing
+        from_state_name = nfa.getStateName(state)
+        to_state_name = nfa.getStateName(to_state) if len(to_state) > 0 else 'ϕ'
+
+        # Split state names by comma, sort them, and join them
+        from_state_name = ','.join(sorted(from_state_name.split(',')))
+        to_state_name = ','.join(sorted(to_state_name.split(',')))
+
+
+        # Print the transition function
+        print(f"The δ' transition for state {from_state_name} is obtained as:")
+        print(f"δ'({from_state_name}, {alphabet}) = {to_state_name}")
+
+    # Add a newline for clarity
+    print()
+
+# Additional code snippet provided by the user
+
+# <insert the code snippet here>
+# Additional code snippet provided by the user
---
+++ Re_to_NFA_main.py
@@ -0,0 +1,253 @@
+import json
+import sys
+from graphviz import Digraph
+
+non_symbols = ['+', '*', '.', '(', ')']
+nfa = {}
+
+class charType:
+    SYMBOL = 1
+    CONCAT = 2
+    UNION  = 3
+    KLEENE = 4
+
+class NFAState:
+    def __init__(self):
+        self.next_state = {}
+
+class ExpressionTree:
+    def __init__(self, charType, value=None):
+        self.charType = charType
+        self.value = value
+        self.left = None
+        self.right = None
+
+def make_exp_tree(regexp):
+    stack = []
+    for c in regexp:
+        if c == "+":
+            z = ExpressionTree(charType.UNION)
+            z.right = stack.pop()
+            z.left = stack.pop()
+            stack.append(z)
+        elif c == ".":
+            z = ExpressionTree(charType.CONCAT)
+            z.right = stack.pop()
+            z.left = stack.pop()
+            stack.append(z)
+        elif c == "*":
+            z = ExpressionTree(charType.KLEENE)
+            z.left = stack.pop() 
+            stack.append(z)
+        elif c == "(" or c == ")":
+            continue  
+        else:
+            stack.append(ExpressionTree(charType.SYMBOL, c))
+    return stack[0]
+
+def compPrecedence(a, b):
+    p = ["+", ".", "*"]
+    return p.index(a) > p.index(b)
+
+def compute_regex(exp_t):
+    # returns E-NFA
+    if exp_t.charType == charType.CONCAT:
+        return do_concat(exp_t)
+    elif exp_t.charType == charType.UNION:
+        return do_union(exp_t)
+    elif exp_t.charType == charType.KLEENE:
+        return do_kleene_star(exp_t)
+    else:
+        return eval_symbol(exp_t)
+
+def eval_symbol(exp_t):
+    start = NFAState()
+    end = NFAState()
+    start.next_state[exp_t.value] = [end]
+    return start, end
+
+def do_concat(exp_t):
+    left_nfa  = compute_regex(exp_t.left)
+    right_nfa = compute_regex(exp_t.right)
+    left_nfa[1].next_state['e'] = [right_nfa[0]]  # Replace '$' with 'e'
+    return left_nfa[0], right_nfa[1]
+
+def do_union(exp_t):
+    start = NFAState()
+    end = NFAState()
+    first_nfa = compute_regex(exp_t.left)
+    second_nfa = compute_regex(exp_t.right)
+    start.next_state['e'] = [first_nfa[0], second_nfa[0]]  # Replace '$' with 'e'
+    first_nfa[1].next_state['e'] = [end]  # Replace '$' with 'e'
+    second_nfa[1].next_state['e'] = [end]  # Replace '$' with 'e'
+    return start, end
+
+def do_kleene_star(exp_t):
+    start = NFAState()
+    end = NFAState()
+    starred_nfa = compute_regex(exp_t.left)
+    start.next_state['e'] = [starred_nfa[0], end]  # Replace '$' with 'e'
+    starred_nfa[1].next_state['e'] = [starred_nfa[0], end]  # Replace '$' with 'e'
+    return start, end
+
+def arrange_transitions(state, states_done, symbol_table):
+    global nfa
+    if state in states_done:
+        return
+    states_done.append(state)
+    for symbol in list(state.next_state):
+        if symbol not in nfa['letters']:
+            nfa['letters'].append(symbol)
+        for ns in state.next_state[symbol]:
+            if ns not in symbol_table:
+                symbol_table[ns] = sorted(symbol_table.values())[-1] + 1
+                q_state = "q" + str(symbol_table[ns])  # Change state to lowercase
+                nfa['states'].append(q_state)
+            nfa['transition_function'].append(["q" + str(symbol_table[state]), 'e' if symbol == '$' else symbol, "q" + str(symbol_table[ns])])  # Replace '$' with 'e'
+        for ns in state.next_state[symbol]:
+            arrange_transitions(ns, states_done, symbol_table)
+
+def notation_to_num(str):
+    return int(str[1:])
+
+def final_st_dfs():
+    global nfa
+    for st in nfa["states"]:
+        count = 0
+        for val in nfa['transition_function']:
+            if val[0] == st and val[2] != st:
+                count += 1
+        if count == 0 and st not in nfa["final_states"]:
+            nfa["final_states"].append(st)
+
+def arrange_nfa(fa):
+    global nfa
+    nfa['states'] = []
+    nfa['letters'] = []
+    nfa['transition_function'] = []
+    nfa['start_states'] = []
+    nfa['final_states'] = []
+    q_1 = "q" + str(1)  # Change state to lowercase
+    nfa['states'].append(q_1)
+    arrange_transitions(fa[0], [], {fa[0] : 1})
+    st_num = [notation_to_num(i) for i in nfa['states']]
+    nfa["start_states"].append("q1")  # Change state to lowercase
+    final_st_dfs()
+
+def add_concat(regex):
+    global non_symbols
+    l = len(regex)
+    res = []
+    for i in range(l - 1):
+        res.append(regex[i])
+        if regex[i] not in non_symbols:
+            if regex[i + 1] not in non_symbols or regex[i + 1] == '(':
+                res += '.'
+        if regex[i] == ')' and regex[i + 1] == '(':
+            res += '.'
+        if regex[i] == '*' and regex[i + 1] == '(':
+            res += '.'
+        if regex[i] == '*' and regex[i + 1] not in non_symbols:
+            res += '.'
+        if regex[i] == ')' and regex[i + 1] not in non_symbols:
+            res += '.'
+
+    res += regex[l - 1]
+    return res
+
+def compute_postfix(regexp):
+    stk = []
+    res = ""
+    for c in regexp:
+        if c not in non_symbols or c == "*":
+            res += c
+        elif c == ")":
+            while len(stk) > 0 and stk[-1] != "(":
+                res += stk.pop()
+            stk.pop()
+        elif c == "(":
+            stk.append(c)
+        elif len(stk) == 0 or stk[-1] == "(" or compPrecedence(c, stk[-1]):
+            stk.append(c)
+        else:
+            while len(stk) > 0 and stk[-1] != "(" and not compPrecedence(c, stk[-1]):
+                res += stk.pop()
+            stk.append(c)
+    while len(stk) > 0:
+        res += stk.pop()
+    return res
+
+def polish_regex(regex):
+    reg = add_concat(regex)
+    regg = compute_postfix(reg)
+    return regg
+
+def load_regex():
+    with open(sys.argv[1], 'r') as inpjson:
+        regex = json.loads(inpjson.read())
+    return regex
+
+def draw_nfa_graph():
+    global nfa
+    dot = Digraph()
+    dot.attr(rankdir='LR')
+    dot.node('start', shape='point')
+    dot.edge('start', nfa["start_states"][0])
+    for transition in nfa['transition_function']:
+        dot.edge(transition[0], transition[2], label=transition[1])
+    for state in nfa["final_states"]:
+        dot.node(state, shape='doublecircle')
+    dot.render('nfa_graph', format='png', cleanup=True)
+
+
+    
+
+def output_nfa_to_json(filename):
+    global nfa
+    states = nfa['states']
+    transition_function = nfa['transition_function']
+    start_states = nfa['start_states']
+    final_states = nfa['final_states']
+    letters = nfa['letters']
+
+    # Convert states to lowercase and replace '$' with 'e'
+    states = [state.lower() for state in states]
+    start_states = [state.lower() for state in start_states]
+    final_states = [state.lower() for state in final_states]
+    transition_function = [[s[0].lower(), 'e' if s[1] == '$' else s[1], s[2].lower()] for s in transition_function]
+
+    # Construct the dictionary
+    nfa_dict = {
+        "no_state": len(states),
+        "states": states,
+        "no_alphabet": len(letters),
+        "alphabets": letters,
+        "start": start_states[0],
+        "no_final": len(final_states),
+        "finals": final_states,
+        "no_transition": len(transition_function),
+        "transitions": transition_function
+    }
+
+    # Write the dictionary to a JSON file
+    with open(filename, 'w') as json_file:
+        json.dump(nfa_dict, json_file, indent=2)
+
+
+def load_regex_from_input():
+    regex = input("Enter RE:")
+    return {"regex": regex}
+
+if __name__ == "__main__":
+    r = load_regex_from_input()
+    reg = r['regex']
+    
+    pr = polish_regex(reg)
+    et = make_exp_tree(pr)
+    fa = compute_regex(et)
+    arrange_nfa(fa)
+    
+    # Specify the filename here
+    output_filename = "output.json"
+    
+    output_nfa_to_json(output_filename)
